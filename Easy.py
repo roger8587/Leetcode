d845b779026d754f903f2dc72f39a5fd6a9736f0
roger8587 <63738826+roger8587@users.noreply.github.com>
2022-03-11 14:36:58 +0800

    Update Easy.py

--- Easy.py
+++ Easy.py
@@ -21,3 +21,32 @@ class Solution:
             if odd:
                 result.append(odd.pop(0))
         return result
+    
+#141. Linked List Cycle
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        slow, fast = head, head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if fast==slow:
+                return True
+        return False
+    
+#21. Merge Two Sorted Lists
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        head = ListNode(0)
+        move = head
+        if not list1: return list2
+        if not list2: return list1
+        while list1 and list2:
+            if list1.val < list2.val:
+                move.next = list1
+                list1 = list1.next
+            else:
+                move.next = list2
+                list2 = list2.next
+            move = move.next
+        move.next = list1 if list1 else list2
+        return head.next
